1180818413ed6d367323f9b4647378800d1a2107
cycyhoang <chychyhoang@gmail.com>
2025-12-09 19:05:59 +0700

    chi update order shipper

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,8 @@ import { Notification } from './notifications/entities/notification.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        socketPath: '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock', 
+        host: configService.get<string>('DB_HOST')!, 
+        port: +configService.get<number>('DB_PORT')!,
         username: configService.get<string>('DB_USER')!,
         password: configService.get<string>('DB_PASS')!,
         database: configService.get<string>('DB_NAME')!,
